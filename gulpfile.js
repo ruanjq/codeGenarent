268f0439366db2d7a8e5ff1a4f7f9c066515e306
DESKTOP-2MPH7MB\ruanj <1016744743@qq.com>
2017-10-26 20:47:23 +0800

    初始化项目

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require("gulp"),
+    rename = require('gulp-rename'),
+    mincss = require("gulp-minify-css"),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    less = require("gulp-less"),
+    uglify = require('gulp-uglify');
+
+const processors = [
+    autoprefixer({
+        browsers: ["last 4 versions", "Firefox >= 20", "Firefox < 20"]
+    })
+];
+gulp.task('less',  function(done) {
+    gulp.src('./css/*.less')
+        .pipe(less())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(mincss())
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./css'));
+    done();
+});
+
+gulp.task('bundle', function() {
+    gulp.src("./lib/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch(['css/*.less'],['less']);
+});
+
+gulp.task('default', ['less','bundle']);
